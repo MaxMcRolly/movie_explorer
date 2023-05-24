d4f926138a361c230391128227e6943ff1908957
mprilutski <mprilutski@mapsoft.by>
2023-05-24 21:09:08 +0300

    updated movie card

--- lib/widgets/movie_card.dart
+++ lib/widgets/movie_card.dart
@@ -8,27 +8,60 @@ class MovieResultCard extends StatelessWidget {
   MovieShort movie;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 56,
-      padding: EdgeInsets.all(8),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Image.network(
-            movie.image!,
-            height: 40,
-          ),
-          SizedBox(
-            width: 8,
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(movie.title!),
-              Text(movie.description!),
-            ],
-          )
-        ],
+    return InkWell(
+      onTap: () async {
+        await Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => MovieDetailsScreen(movieId: movie.id!)));
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          color: Color(0xff454547),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        // height: 127,
+        padding: EdgeInsets.all(8),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              height: 108,
+              width: 125,
+              child: FittedBox(
+                child: CachedImage(url: movie.image!),
+              ),
+            ),
+            SizedBox(
+              width: 8,
+            ),
+            Expanded(
+              child: Column(
+                // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    movie.title!,
+                    style: GoogleFonts.montserrat(
+                        color: Colors.white,
+                        fontSize: 21,
+                        fontWeight: FontWeight.w600),
+                  ),
+                  SizedBox(
+                    height: 32,
+                  ),
+                  Text(
+                    movie.description!,
+                    style: GoogleFonts.montserrat(
+                        color: Colors.white,
+                        fontSize: 14,
+                        fontWeight: FontWeight.w500),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
